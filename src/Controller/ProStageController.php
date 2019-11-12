f4e9f52b8f8a8e5cd5b669b97ba425f7e86f04ef
Christopher Grassi <cgrassi@iutbayonne.univ-pau.fr>
2019-11-12 15:32:40 +0100

    Creation des routes

--- src/Controller/ProStageController.php
+++ src/Controller/ProStageController.php
@@ -4,16 +4,31 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Response;
 
 class ProStageController extends AbstractController
 {
-    /**
-     * @Route("/pro/stage", name="pro_stage")
-     */
+    
     public function index()
     {
-        return $this->render('pro_stage/index.html.twig', [
-            'controller_name' => 'ProStageController',
-        ]);
+        return new Response('<html><body><h1> Bienvenue sur la page d\'accueil de Prostages </h1></body></html>' );
     }
+
+    public function brutEntreprises()
+    {
+        return new Response('<html><body><h1> Cette page affichera la liste des entreprises proposant un stage </h1></body></html>' );
+    }
+
+    public function brutFormations()
+    {
+        return new Response('<html><body><h1> Cette page affichera la liste des formations de l\'IUTs </h1></body></html>' );
+    }
+
+    public function brutStage($id)
+    {
+        return new Response('<html><body><h1> Cette page affichera le descriptif du stage ayant pour identifiant '. $id . ' </h1></body></html>' );
+    }
+
+
+
 }

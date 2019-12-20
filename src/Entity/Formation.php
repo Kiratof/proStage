80aed569e2042fb1f0d137e59de7baa51c1c537c
Christopher Grassi <cgrassi@iutbayonne.univ-pau.fr>
2019-12-20 09:09:18 +0100

    Mise en place des relations bidirectionnelles

--- src/Entity/Formation.php
+++ src/Entity/Formation.php
@@ -2,6 +2,8 @@
 
 namespace App\Entity;
 
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
@@ -30,6 +32,16 @@ class Formation
      * @ORM\Column(type="string", length=50)
      */
     private $nom;
+
+    /**
+     * @ORM\ManyToMany(targetEntity="App\Entity\Stage", mappedBy="formations")
+     */
+    private $stages;
+
+    public function __construct()
+    {
+        $this->stages = new ArrayCollection();
+    }
 
     public function getId(): ?int
     {
@@ -68,6 +80,34 @@ class Formation
     public function setNom(string $nom): self
     {
         $this->nom = $nom;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Stage[]
+     */
+    public function getStages(): Collection
+    {
+        return $this->stages;
+    }
+
+    public function addStage(Stage $stage): self
+    {
+        if (!$this->stages->contains($stage)) {
+            $this->stages[] = $stage;
+            $stage->addFormation($this);
+        }
+
+        return $this;
+    }
+
+    public function removeStage(Stage $stage): self
+    {
+        if ($this->stages->contains($stage)) {
+            $this->stages->removeElement($stage);
+            $stage->removeFormation($this);
+        }
 
         return $this;
     }
